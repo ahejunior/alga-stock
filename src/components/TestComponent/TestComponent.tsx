6dd8aa3255b7345d2706938b2d58138a03a702e3
ahejunior <ahejunior@yahoo.com.br>
2021-06-04 10:30:23 -0300

    hook useEffect ciclo de vida

--- src/components/TestComponent/TestComponent.tsx
+++ src/components/TestComponent/TestComponent.tsx
@@ -1,9 +1,17 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 
 import './TestComponent.css';
 
 function TestComponent(props: {name: string}) {
     const [age, setAge] = useState(18);
+
+    useEffect( () => {
+        console.log('component was created')
+    }, [])
+
+    useEffect( () => {
+        console.log('Age was updated to ' + age)
+    }, [age])
 
     return <div className="TestComponentStyle">
         Hello {props.name}, {age}<br/>

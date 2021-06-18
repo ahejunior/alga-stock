e1f07360a4fb6e6397b87fe17a8e6f97ee02a201
ahejunior <ahejunior@yahoo.com.br>
2021-06-18 15:30:18 -0300

    eslint-disable products.reducer

--- src/redux/Products/Products.reducer.ts
+++ src/redux/Products/Products.reducer.ts
@@ -1,6 +1,7 @@
 import { Product } from "../../shared/Table/Table.mockdata"
 import { Action } from ".."
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default function (state: Product[] = [], action: Action): Product[] {
   switch (action.type) {
     case 'FETCH_PRODUCTS':
